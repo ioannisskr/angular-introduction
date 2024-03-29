29d541e2732eac92389616c5e59b68830711d25c
ioannisskr <ayeegiannos@gmail.com>
2024-03-29 17:05:04 +0200

    One way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Ioannis';
+
+  person = {
+    givenName: "Ioannis",
+    surName: "Skretis",
+    age: 44,
+    email: "ayeegiannos@gmail.com"
+  }
 }
