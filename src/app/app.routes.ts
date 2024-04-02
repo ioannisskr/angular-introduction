c043cdef18f1f577112a3e98cac2aa28c5252111
ioannisskr <ayeegiannos@gmail.com>
2024-04-02 19:43:14 +0300

    component output

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
 import { ComponentForDirectiveExampleComponent } from './components/component-for-directive-example/component-for-directive-example.component';
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
+import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
 
 export const routes: Routes = [
     {path: 'component-input-example', component: ComponentInputExampleComponent},
     {path: 'for-directive-example', component: ComponentForDirectiveExampleComponent},
     {path: 'event-bind-example', component: EventBindExampleComponent},
     {path: 'simple-datatable-example', component: SimpleDatatableExampleComponent},
+    {path: 'component-output-example', component: ComponentOutputExampleComponent},
     {path: '', component: WelcomeComponent},
 ];
